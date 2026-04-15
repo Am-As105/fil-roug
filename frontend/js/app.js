0dd3fa31dd0c6c735a07128b0bb76cdff326a387
amine <aminear220@gmail.com>
2026-04-15 16:11:55 +0100

    feat: add map integration with Leaflet and display disaster markers

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -22,4 +22,36 @@ if (addDisasterLinks.length && addDisasterSection) {
       }
     });
   });
+  
 }
+const map = L.map('map').setView([31.7917, -7.0926], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+}).addTo(map);
+
+L.marker([33.5731, -7.5898]).addTo(map)
+  .bindPopup("Casablanca")
+  .openPopup();
+
+const redIcon = L.icon({
+  iconUrl: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32]
+});
+
+L.marker([31.6295, -7.9811], { icon: redIcon })
+  .addTo(map)
+  .bindPopup("Séisme - Al Haouz");
+
+L.marker([33.5731, -7.5898], { icon: redIcon })
+  .addTo(map)
+  .bindPopup("Inondation - Casablanca");
+
+L.marker([34.0331, -5.0003], { icon: redIcon })
+  .addTo(map)
+  .bindPopup("Incendie - Taza");
+
+L.marker([35.1688, -5.2636], { icon: redIcon })
+  .addTo(map)
+  .bindPopup("Glissement - Chefchaouen");
