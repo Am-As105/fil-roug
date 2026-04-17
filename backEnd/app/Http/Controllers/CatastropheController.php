e4571f413954b2bd4eb66226413fb2283657a4d5
amine <aminear220@gmail.com>
2026-04-17 10:13:22 +0100

    Add store method to create catastrophe

---
+++ backEnd/app/Http/Controllers/CatastropheController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catastrophe;
+use Illuminate\Http\Request;
+
+class CatastropheController extends Controller
+{
+    //
+    // public function index()
+    // {
+    //     $catastrophes = Catastrophe::with('type')->get();
+
+    //     return response()->json($catastrophes);
+
+    // }
+    public function store(Request $request )
+    {
+        $catastrophe = Catastrophe::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'date' => $request->date,
+            'severity' => $request->severity,
+            'status' => $request->status,
+            'type_id' => $request->type_id,
+            
+        ]);
+
+        return response()->json($catastrophe);
+    }
+
+
+    // public function delete($catastropheId)
+    // {
+    //     $catastrophe = Catastrophe::find($catastropheId);
+    //     $catastrophe->delete();
+    //     return response()->json(['message' => 'deleted']);
+    // }
+}
