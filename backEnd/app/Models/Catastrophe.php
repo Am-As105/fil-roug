5279737b40939686f9b0b989094cf81c67c5ca5e
amine <aminear220@gmail.com>
2026-04-17 10:14:17 +0100

    Add destroy method to delete catastrophe

---
+++ backEnd/app/Models/Catastrophe.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Catastrophe extends Model
+{
+    //
+    protected $fillable = [
+        'title',
+        'description',
+        'latitude',
+        'longitude',
+        'date',
+        'severity',
+        'status',
+        'type_id'
+    ];
+    public function type()
+    {
+        return $this->belongsTo(type::class);
+    }
+}
