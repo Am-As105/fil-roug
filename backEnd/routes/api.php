63e332ab1c6d51994fa56086bc5b6329a764d78b
amine <aminear220@gmail.com>
2026-04-25 15:46:25 +0100

    add rout typs

--- backEnd/routes/api.php
+++ backEnd/routes/api.php
@@ -15,10 +15,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/catastrophes', [CatastropheController::class, 'index']);
 Route::get('/catastrophes/{catastropheId}', [CatastropheController::class, 'show']);
+ Route::get('/types', [TypeController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/catastrophes', [CatastropheController::class, 'store']);
     Route::match(['put', 'patch'], '/catastrophes/{catastropheId}', [CatastropheController::class, 'update']);
     Route::delete('/catastrophes/{catastropheId}', [CatastropheController::class, 'delete']);
-    Route::get('/types', [TypeController::class, 'index']);
 });
