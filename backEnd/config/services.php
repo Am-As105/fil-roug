d29b9d3aa8a1d6b17563f4c8d8cec6f01e9bbea4
amine <aminear220@gmail.com>
2026-04-27 09:44:14 +0100

    add setting twillo sms

--- backEnd/config/services.php
+++ backEnd/config/services.php
@@ -2,18 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
@@ -36,12 +24,10 @@ return [
     ],
 
     'twilio' => [
-       'twilio' => [
-    'sid' => env('TWILIO_SID'),
-    'token' => env('TWILIO_TOKEN'),
-    'from' => env('TWILIO_FROM'),
-    'to' => env('TWILIO_TO'),
-],
+        'sid' => env('TWILIO_SID'),
+        'token' => env('TWILIO_TOKEN'),
+        'from' => env('TWILIO_FROM'),
+        'to' => env('TWILIO_TO'),
         'verify_service_sid' => env('TWILIO_VERIFY_SERVICE_SID'),
     ],
 
