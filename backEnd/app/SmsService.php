daee543d1d27ca6e07d13801acb115fdf05a6ba0
amine <aminear220@gmail.com>
2026-04-27 21:20:47 +0100

    fix error (sms deos not work

--- backEnd/app/SmsService.php
+++ backEnd/app/SmsService.php
@@ -10,21 +10,19 @@ class SmsService
     public function send($message)
     {
         try {
-            return Http::withHeaders([
-                'Authorization' => 'App ' . config('services.infobip.key'),
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json'
-            ])->post(config('services.infobip.base_url') . '/sms/3/messages', [
-                "messages" => [
-                    [
-                        "destinations" => [
-                            ["to" => config('services.infobip.to')]
-                        ],
-                        "from" => "InfoSMS",
-                        "text" => $message
-                    ]
-                ]
-            ]);
+           return Http::withHeaders([
+    'Authorization' => 'App ' . config('services.infobip.key'),
+])->post(config('services.infobip.base_url') . '/sms/3/messages', [
+    "messages" => [
+        [
+            "destinations" => [
+                ["to" => config('services.infobip.to')]
+            ],
+            "from" => "ServiceSMS",
+            "text" => $message
+        ]
+    ]
+]);
         } catch (\Throwable $e) {
             Log::error('SMS error: ' . $e->getMessage());
         }
