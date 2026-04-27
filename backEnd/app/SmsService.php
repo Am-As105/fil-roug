2a3a97183596e9b002ae467704f817d2f1cd5b1f
amine <aminear220@gmail.com>
2026-04-27 21:32:22 +0100

    fix error (sms deos not work

--- backEnd/app/SmsService.php
+++ backEnd/app/SmsService.php
@@ -7,34 +7,23 @@ use Illuminate\Support\Facades\Log;
 
 class SmsService
 {
-    public function send($message)
-    {
-        try {
-            $response = Http::withHeaders([
-                'Authorization' => 'App ' . env('INFOBIP_API_KEY'),
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ])->post(env('INFOBIP_BASE_URL') . '/sms/3/messages', [
-                "messages" => [
-                    [
-                        "destinations" => [
-                            [
-                                "to" => env('INFOBIP_TO')
-                            ]
-                        ],
-                        "from" => "447491163443",
-                        "text" => $message
-                    ]
-                ]
-            ]);
+   public function send($message)
+{
+    $response = \Illuminate\Support\Facades\Http::withHeaders([
+        'Authorization' => 'App ' . env('INFOBIP_API_KEY'),
+        'Content-Type' => 'application/json',
+    ])->post(env('INFOBIP_BASE_URL') . '/sms/3/messages', [
+        "messages" => [
+            [
+                "destinations" => [
+                    ["to" => env('INFOBIP_TO')]
+                ],
+                "from" => "447491163443",
+                "text" => $message
+            ]
+        ]
+    ]);
 
-            Log::info('SMS response', [
-                'status' => $response->status(),
-                'body' => $response->body()
-            ]);
-
-        } catch (\Throwable $e) {
-            Log::error('SMS failed: ' . $e->getMessage());
-        }
-    }
+    dd($response->status(), $response->body());
+}
 }
